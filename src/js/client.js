f383dcd05dff10268055da967b10bc1ea280bd4b
micnolmad <micnolmad@gmail.com>
2023-07-23 15:11:57 +0200

    .

--- src/js/client.js
+++ src/js/client.js
@@ -1,9 +1,12 @@
 window.TrelloPowerUp.initialize({
-  'card-buttons': function(t, opts){
+  'card-buttons': function (t, opts){
     return [{
       icon: '/src/images/mimir.svg',
       text: 'Estimate Size',
-      condition: 'always'
+      condition: 'always',
+      callback: function (t) {
+        return t.card().then(card => alert('hello world'))
+      }
     }];
   },
 });
