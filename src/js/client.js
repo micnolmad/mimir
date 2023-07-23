acc8d9027cf30514fea7ca0e3076c33e714bc685
micnolmad <micnolmad@gmail.com>
2023-07-23 14:01:08 +0200

    .

--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,5 @@
-TrelloPowerUp.initialize({
-  'card-buttons': function(t, options){
+window.TrelloPowerUp.initialize({
+  'card-buttons': function(t, opts){
     return [{
       text: 'Estimate Size',
     }];
