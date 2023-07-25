34d1d4749ecf8e0677987d018b9f3b28b9c3d5f6
micnolmad <micnolmad@gmail.com>
2023-07-25 22:39:19 +0200

    .

--- js/client.js
+++ js/client.js
@@ -5,18 +5,19 @@ var mimirGetTime = function() {
 window.TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
     return [{
-      icon: '/images/mimir.svg',
-      text: 'Estimate Size',
-      condition: 'always',
-      callback: function (t) {
+      icon: '/images/mimir.svg'
+      ,text: 'Estimate Size'
+      ,condition: 'always'
+      ,callback: function (t) {
         return t.card().then(card => alert('hello world!'))
       }
     }];
   },
-  'card-badges': function (t, opts, test) {
+  'card-badges': function (t, opts) {
     return [{
-      text: '5',
-      color: 'sky'
+      text: mimirGetTime()
+      ,color: 'sky'
+      ,condition: 'always'
     }];
   }
 });
