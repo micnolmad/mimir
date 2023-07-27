302722f0d962affe0635e0196d6464c7087d3c5c
micnolmad <micnolmad@gmail.com>
2023-07-27 22:57:01 +0200

    .

--- js/client.js
+++ js/client.js
@@ -16,7 +16,7 @@ window.TrelloPowerUp.initialize({
   }
   ,'card-badges': function (t, opts) {
     return t.card("all").then(function (card) {
-      console.log(JSON.stringify(card, null, 2));
+      console.log(card('start'));
     });
   }
 });
